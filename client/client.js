0feb0346d1f237776c4ff272c5fcc42ac325b993
mehulsatardekar <mehulsatardekar1999@gmail.com>
2021-03-07 00:17:59 +0530

    route changed in index file

--- client/client.js
+++ client/client.js
@@ -25,7 +25,7 @@ async function send() {
 
   // Send Push Notification
   console.log("Sending Push...");
-  await fetch("https://mehulsatardekar.github.io/subscribe/subscribe", {
+  await fetch("https://mehulsatardekar.github.io/subscribe", {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
