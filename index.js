7a263b4be3316501f164f4e9f9682252e84b549c
mehulsatardekar <mehulsatardekar1999@gmail.com>
2021-03-06 23:48:04 +0530

    route changed in index file

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ webpush.setVapidDetails(
   );
   
   // Subscribe Route
-  app.post(" https://mehulsatardekar.github.io/subscribe", (req, res) => {
+  app.post("/subscribe", (req, res) => {
     // Get pushSubscription object
     const subscription = req.body;
   
