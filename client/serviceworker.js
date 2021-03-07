c5dea3a49716cb5645709829cbfff232844fd19d
mehulsatardekar <mehulsatardekar1999@gmail.com>
2021-03-07 17:50:12 +0530

    audio file and vibration mode added

--- client/serviceworker.js
+++ client/serviceworker.js
@@ -1,10 +1,15 @@
 console.log("Service Worker Loaded...");
 
+// Note that while adding iamges and icons make sure they serve on http/https otherwise they wont work.
 self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Traversy Media!",
-    image: "https://raw.githubusercontent.com/mehulsatardekar/push-notifier/main/splash.png"
+    body: "This is Mehul and this is working demo of Push Notification services\n okkkkk",
+    image: "https://raw.githubusercontent.com/mehulsatardekar/push-notifier/main/splash.png",
+    icon:"https://raw.githubusercontent.com/mehulsatardekar/push-notifier/main/avatar.jpg",
+    vibrate: [500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40,500]
+
+
   });
 });
